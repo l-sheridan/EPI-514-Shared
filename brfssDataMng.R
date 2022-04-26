565385702d4d0885c9c7ee7570c57be8b5e9974f
keelyparis <104037107+keelyparis@users.noreply.github.com>
2022-04-26 12:49:03 -0700

    Update brfssDataMng.R

--- brfssDataMng.R
+++ brfssDataMng.R
@@ -27,33 +27,48 @@ variable.names(combo)
     #note: if you find a variable in the codebook that is NOT included in the subset
     #below but you want it, just add it to the subset code
 
-brfss <- subset(combo, select = c(year, X_ststr, sex, age, X_ageg5yr, X_age65yr,
-                                  X_age_g, X_hispanc, educa, X_race, X_imprace,
-                                  employ1, genhlth, X_racegr3, X_m_race, X_prace1,
-                                  X_phys2, hlthpln1, medicare,
-                                  hlthcvr1,smoke100, smokday2, X_smoker3, X_rfsmok3, X_rfdrhv7,
+brfss <- subset(combo, select = c(year, X_ststr, sex, age,
+                                  X_age_g, educa, X_race, X_imprace,
+                                  genhlth, X_racegr3, X_mrace1, X_prace1,
+                                  hlthpln1,
+                                  smokday2, X_smoker3, X_rfsmok3, X_rfdrhv7,
                                   mjever, mj30day, op_any, op_days, op_more,
                                   op_norx, op_less, occ11, ind11, 
-                                  jobactiv, emplinjm, injpay, injpay1,
+                                  jobactiv, emplinjm,
                                   X_llcpwt, use_wt, use_comb, agestd, agestdwt))
         #only included some weighting variables, probably a good idea to double
         #check before we weight things
-
+brfss <- brfss[brfss$year=="2019",]
 
       #delete big  data frame for space (after subsetting)
 combo <- NA
 
-##################################################
-##explore data, tabulate industry and occupation##
-##################################################
+###############################################
+##explore data, tabulate exposure and outcome##
+###############################################
 View(brfss)
+
+  #exposure tabulations
+table(brfss$occ11, useNA="ifany")
+table(brfss$ind11, useNA="ifany") 
+
+   #outcome tabulations
+table(brfss$op_any, useNA="ifany")
 table(brfss$op_more, useNA="ifany")
 
-table(brfss$occ11, useNA="ifany")
-table(brfss$ind11, useNA="ifany")
+table(brfss$X_rfdrhv7, useNA="ifany")
+
+    #exposure tabulation by year
+brfss %>% filter(year=="2019") %>% pull(occ11) %>% table()
+brfss %>% filter(year=="2020") %>% pull(occ11) %>% table()
+
+    #amount of heavy drinkers who also answered opioid module
+brfss %>% filter(year=="2019") %>% filter(op_any!= "Refused" & op_any!="") %>% pull(X_rfdrhv7) %>% table()
+brfss %>% filter(year=="2020") %>% filter(op_any!= "Refused"& op_any!="") %>% pull(X_rfdrhv7) %>% table()
+
 
 ########################################################################################
-##create exposure variables (one using 3 sector model, one using blue vs white collar)##
+##create exposure variables (one using 3 sector model, one using manual vs non-manual)##
 ########################################################################################
 
 brfss$sector[brfss$occ11=="Farming, Fishing, Forestry" | brfss$occ11== "Production"] <- "primary"
@@ -71,7 +86,16 @@ brfss$job[brfss$occ11=="Farming, Fishing, Forestry" | brfss$occ11== "Production"
                brfss$occ11=="Service"] <- "manual"
 brfss$job[brfss$occ11=="Mgt, business/finance" | brfss$occ11=="Professional & related" |
                brfss$occ11=="Sales & related"| brfss$occ11=="Office & admin"] <- "nonManual"
+
+
+    #creating 0/1 version of job with manual as exposed
+brfss$job01[brfss$job=="manual"] <- 1
+brfss$job01[brfss$job=="nonManual"] <- 0
      
+
+    #creating 1/2 version of job with manual as exposed (1)
+brfss$job12[brfss$job=="manual"] <- 1
+brfss$job12[brfss$job=="nonManual"] <- 2
 
 ############################
 ##create outcome variables##
@@ -82,6 +106,48 @@ brfss$outcome[brfss$op_any=="Yes" & brfss$X_rfdrhv7=="Yes"] <- 1
 brfss$outcome[brfss$op_any=="No" & brfss$X_rfdrhv7=="No"] <- 0
         #they neither used op in the past year nor are a heavy drinker 
 
+    #cleaning op_any variable and op_more variable
+brfss$op_any[brfss$op_any=="DK" | brfss$op_any=="Refused" | brfss$op_any==""] <- NA
+brfss$op_more[brfss$op_more=="DK" | brfss$op_more=="Refused" | brfss$op_more==""] <- NA
+
+
+    #creating 0/1 version of op_any and op_more
+brfss$opAny01[brfss$op_any=="Yes"] <- 1
+brfss$opAny01[brfss$op_any=="No"] <- 0
+
+brfss$opMore01[brfss$op_more=="Yes"] <- 1
+brfss$opMore01[brfss$op_more=="No"] <- 0
+
+
+    #creating 1/2 version of op_any and op_more
+brfss$opAny12[brfss$op_any=="Yes"] <- 1
+brfss$opAny12[brfss$op_any=="No"] <- 2
+
+brfss$opMore12[brfss$op_more=="Yes"] <- 1
+brfss$opMore12[brfss$op_more=="No"] <- 2
+
+#################
+#Reorder Columns#
+#################
+order <- c("year", "sex", "occ11", "ind11", "job", "job01", "job12", "op_any", "opAny01",
+           "opAny12", "op_more", "opMore01", "opMore12", "age", "X_age_g", "hlthpln1",
+           "X_racegr3", "X_race", "X_prace1","X_mrace1", "X_imprace", "smokday2", "X_smoker3",
+           "X_rfsmok3","educa" ,"hsComplete", "X_ststr", "X_llcpwt", "op_days", "op_more", "op_norx", "op_less",
+           "genhlth", "mjever", "mj30day", "jobactiv", "emplinjm", "use_wt", "use_comb",
+           "agestd", "agestdwt", "sector", "X_rfdrhv7", "outcome")
+
+brfss <- brfss[, order]
+
+#########################################
+#Recoding Missings & Recoding covariates#
+#########################################
+brfss$emplinjm[brfss$emplinjm=="Refused" | brfss$emplinjm=="DK" | brfss$emplinjm==""]<- NA
+brfss$hlthpln1[brfss$hlthpln1=="Refused" | brfss$hlthpln1=="DK"]<- NA
+
+brfss$educa[brfss$educa=="Refused"] <- NA
+brfss$hsComplete <- ifelse(brfss$educa=="Grade 1-8" | brfss$educa=="Grade 9-11", 0, 1)
+
+
 
 #####################################
 ##tabulate new variables we created##
@@ -90,8 +156,9 @@ table(brfss$sector, useNA="ifany")
 table(brfss$job, useNA="ifany")
 
     #crosstabs
-table(brfss$op_more,  brfss$job, deparse.level=2)
 table(brfss$op_any, brfss$job, deparse.level=2)
+table(brfss$op_more, brfss$job, deparse.level=2)
+
 
 table(brfss$job, brfss$outcome, deparse.level = 2)
 
@@ -103,45 +170,124 @@ brfss$emplinjm[brfss$emplinjm=="Refused" | brfss$emplinjm=="DK" | brfss$emplinjm
 
 (rrTabStrat <- table(brfss$job, brfss$outcome, 
                      brfss$emplinjm, deparse.level=2))
+(stratInj <- table(brfss$job, brfss$op_any, brfss$emplinjm, deparse.level=2))
 
-    #hispanic
-brfss$X_hispanc[brfss$X_hispanc=="Refused"] <- NA
-(stratHisp <- table(brfss$job, brfss$outcome, brfss$X_hispanc, deparse.level = 2))
 
     #are they insured
 brfss$hlthpln1[brfss$hlthpln1=="Refused" | brfss$hlthpln1=="DK"]<- NA
+
 (stratInsr <- table(brfss$job, brfss$outcome, brfss$hlthpln1, deparse.level=2))
+(stratInsrOp <- table(brfss$job, brfss$op_any, brfss$hlthpln1, deparse.level=2))
 
     #race group
 (stratRace <- table(brfss$job, brfss$outcome, brfss$X_prace1, deparse.level=2))
 
+
+    #age
+(stratAge <- table(brfss$job, brfss$op_any, brfss$X_age_g, deparse.level=2))
+
+    #educa
+brfss$educa[brfss$educa=="Refused"] <- NA
+table(brfss$educa) 
+
+(stratEd <- table(brfss$job, brfss$op_any, brfss$educa, deparse.level=2))
+
+#######################
+#Export Clean-ish Data#
+#######################
+write.csv(brfss, "brfssClean.csv")
+
+
 ##############
 ##power calc##
 ##############
-    #prev in unexposed (white collar) is like 0.6%, but I am eyeballing the table I found
+    #prev of op non manual of 12%, manual sample size of 1500
 epi.ssxsectn(
   pdexp1=NA,
-  pdexp0=.006,
+  pdexp0=.12,
   n=5710,
   power=0.8,
-  r=1198/4512
+  r=1500/4210
 )
 
-#####################################################################################
-#separate data frame with no missing/refused/DK data for any opioid use in past year# 
-#####################################################################################
-    #note: not entirely necessary now that we have a separate outcome variable
-    #that already counted these as missing, but I wanted to keep the code just in
-    #case, plus the power calc is slightly different so I want that somewhere
-complete <- brfss[brfss$op_any!="Refused" & brfss$op_any!="DK",]
-table(complete$job, useNA="ifany")
-
+  #prev of op non manual of 12.5%, manual sample size of 1500
 epi.ssxsectn(
   pdexp1=NA,
-  pdexp0=.006,
-  n=5694,
+  pdexp0=.125,
+  n=5710,
   power=0.8,
-  r=1192/4502
+  r=1500/4210
+)
+
+  #prev of op non manual of 13%, manual sample size of 1500
+epi.ssxsectn(
+  pdexp1=NA,
+  pdexp0=.13,
+  n=5710,
+  power=0.8,
+  r=1500/4210
+)
+
+  #prev of op non manual of 12%, manual sample size of 2000
+epi.ssxsectn(
+  pdexp1=NA,
+  pdexp0=.12,
+  n=5710,
+  power=0.8,
+  r=2000/3710
+)
+
+  #prev of op non manual of 12.5%, manual sample size of 2000
+epi.ssxsectn(
+  pdexp1=NA,
+  pdexp0=.125,
+  n=5710,
+  power=0.8,
+  r=2000/3710
+)
+
+  #prev of op non manual of 13%, manual sample size of 2000
+epi.ssxsectn(
+  pdexp1=NA,
+  pdexp0=.13,
+  n=5710,
+  power=0.8,
+  r=2000/3710
+)
+
+  #prev of op non manual of 12%, manual sample size of 2500
+epi.ssxsectn(
+  pdexp1=NA,
+  pdexp0=.12,
+  n=5710,
+  power=0.8,
+  r=2500/3210
+)
+
+  #prev of op non manual of 12.5%, manual sample size of 2500
+epi.ssxsectn(
+  pdexp1=NA,
+  pdexp0=.125,
+  n=5710,
+  power=0.8,
+  r=2500/3210
+)
+
+  #prev of op non manual of 13%, manual sample size of 2500
+epi.ssxsectn(
+  pdexp1=NA,
+  pdexp0=.13,
+  n=5710,
+  power=0.8,
+  r=2500/3210
+)
+
+
+    #2018 manual laborers (to calc prev in unexposed)
+326+200+188+264+11+789+117
+  #total 5494; nonmanual 3599
+
+
 )
 
 
